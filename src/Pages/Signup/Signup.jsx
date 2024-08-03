edc8604b120369319e61a5e10f3ac89f614d415c
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2024-08-03 10:12:37 +0530

    push signup

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -13,12 +13,10 @@ function Signup() {
         <div>
 
             <Grid container  >
-                <Grid item  xs={12} sm={12} md={12} lg={12} sx={{ display: "flex", flexDirection: "column" }}>
-
-                    <Grid item  xs={6} sm={6} md={6} lg={6}  sx={{ border: "2px solid black" , backgroundColor:"pink"}}>
-
+                <Grid item xs={12} sm={12} md={12} lg={12} sx={{ display: "flex", flexDirection: "column" }}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} sx={{ border: "2px solid black", backgroundColor: "pink" }}>
                         <Grid item  >
-                            <Grid item xs={10} sm={10} md={10} lg={10} spacing={2} sx={{ backgroundColor: "red" }} >
+                            <Grid item  spacing={2} sx={{ backgroundColor: "red" }} >
                                 <Grid item  >
                                     <Typography variant='h2'>Registration</Typography>
                                     <Typography variant='h5'>Create New RTMS Account</Typography>
@@ -33,11 +31,7 @@ function Signup() {
                                                 </Box>
                                             </Grid>
                                             <Grid>
-
                                                 <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                                    {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
-
-
                                                     <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} fontSize='large' />
 
                                                     <TextField id="input-with-sx" label="Password" variant="standard" />
@@ -48,65 +42,6 @@ function Signup() {
                                 </Grid>
                             </Grid>
 
-                        </Grid>
-
-
-                        <Grid item sx={{ backgroundColor: "pink" }}  >
-                            <Grid item sx={{ backgroundColor: "red" }} >
-                                <Grid item  >
-                                    <Typography variant='h2'>Registration</Typography>
-                                    <Typography variant='h5'>Create New RTMS Account</Typography>
-                                </Grid>
-                                <Grid>
-                                    <form>
-                                        <Grid item mt={5} gap='9px' style={{ display: 'flex', flexDirection: 'column' }}>
-                                            <Grid >
-                                                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                                    <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} fontSize='large' />
-                                                    <TextField id="input-with-sx" label="Username" variant="standard" />
-                                                </Box>
-                                            </Grid>
-                                            <Grid>
-
-                                                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                                    {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
-
-
-                                                    <HttpsIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} fontSize='large' />
-
-                                                    <TextField id="input-with-sx" label="Password" variant="standard" />
-                                                </Box>
-                                            </Grid>
-                                        </Grid>
-                                    </form>
-                                </Grid>
-                            </Grid>
-
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} sm={12} md={12} lg={12} sx={{ border: "2px solid black", backgroundColor: "yellow" }} >
-                        <Grid className='.l-form'></Grid>
-                    </Grid>
-                    {/* <Grid item xs={4} sm={4} md={4} lg={4} className='.l-form' sx={{border:"2px solid black"}} >
-                        <Grid className='.l-form'></Grid>
-                    </Grid> */}
-
-                </Grid>
-
-                <Grid container spacing={2}>
-
-                    <Grid container mt={'85px'}>
-                        <Grid item lg={12} md={12} sm={12} xs={12} sx={{ justifyContent: 'center', pl: '200px' }} >
-                            <img src={wifi} alt="" height={'100px'} />
-                        </Grid>
-                        <Grid item lg={12} md={12} sm={12} xs={12} sx={{ pl: '102px' }}>
-
-                            <img src={wifi} alt="" height={'100px'} />
-                            <img src={wifi} alt="" height={'100px'} />
-                            <img src={wifi} alt="" height={'100px'} />
-                        </Grid>
-                        <Grid item lg={12} md={12} sm={12} xs={12} sx={{ pl: '200px' }}>
-                            <img src={wifi} alt="" height={'100px'} />
                         </Grid>
                     </Grid>
 
