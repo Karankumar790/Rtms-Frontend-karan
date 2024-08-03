44c5fa8c94afe0189ede6562b8bad6b65ceb6054
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2024-08-03 16:03:49 +0530

    signup resposive

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route, useRoutes } from 'react-router-dom'
 import { lazy } from 'react';
-import Header from './components/Header/Header.jsx';
-import Footer from './components/Footer/Footer.jsx';
+// import Header from './components/Header/Header.jsx';
+// import Footer from './components/Footer/Footer.jsx';
 
 import Signup from './Pages/Signup/Signup.jsx';
 const Login = lazy(() => import('./Pages/Login/Login.jsx'))
 
-// const Header = lazy(() => import('./components/Header/Header.jsx'))
+const Dashboard = lazy(() => import('./Dashboard/Dash.jsx'))
 
-// const Footer = lazy(() => import('./components/Footer/Footer.jsx'))
+const Header = lazy(() => import('./components/Header/Header.jsx'))
+
+const Footer = lazy(() => import('./components/Footer/Footer.jsx'))
 
 function App() {
   const route = useRoutes([
 
     { path: '/', element: <Login /> },
-    { path: '/Signup', element: <Signup /> }
+    { path: '/Signup', element: <Signup /> },
+    { path: '/Dashboard', element: <Dashboard /> }
 
   ])
 
   return (
     <>
-        <Header />
       <Suspense fallback={'LOADING......'}>
+        <Header />
 
         {route}
 
-      </Suspense>
         <Footer />
+      </Suspense>
     </>
   )
 }
