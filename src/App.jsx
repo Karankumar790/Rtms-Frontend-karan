add8ebda44f1e98ff2415b964cef0c4c964bbc9c
ShivamSaini16 <shivamsaini0852@gmail.com>
2024-08-05 21:59:56 -0700

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React, { Suspense, lazy } from 'react';
 import { useRoutes } from 'react-router-dom'
 import AppSk from './components/Skeletons/AppSk.jsx';
+import Home from './Pages/Dashboard/Home/Home.jsx'
+import SideBar from './Pages/Dashboard/SideBar/SideBar.jsx'
 const Login = lazy(() => import('./Pages/Login/Login.jsx'))
 const Signup = lazy(() => import('./Pages/Signup/Signup.jsx'))
 const Dashboard = lazy(() => import('./Pages/Dashboard/index.jsx'))
 const Forgot = lazy(() => import('./Pages/Forgot/Forgot.jsx'))
 const Reset = lazy(() => import('./Pages/Reset/Reset.jsx'))
+const Table = lazy(() => import('./Pages/Dashboard/Table/Table.jsx'))
 
 
 function App() {
   const route = useRoutes([
     { path: '/', element: <Login /> },
     { path: '/signup', element: <Signup /> },
-    { path: '/dashboard', element: <Dashboard /> },
+    {
+      path: '/dashboard', element: <Dashboard />,
+      children: [
+        { path: "", element: <Home /> },
+        {path:"table",element:<Table/>}
+      ]
+    },
     { path: '/reset', element: <Reset /> },
     { path: '/forgot', element: <Forgot /> }
 
@@ -20,7 +29,7 @@ function App() {
 
   return (
     <>
-      <Suspense fallback={<AppSk/>}>
+      <Suspense fallback={<AppSk />}>
         {route}
       </Suspense>
     </>
