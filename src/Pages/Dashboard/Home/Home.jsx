060da5a79a138082e13591801568f4734c772ea5
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2024-08-10 19:20:20 +0530

    graph

--- src/Pages/Dashboard/Home/Home.jsx
+++ src/Pages/Dashboard/Home/Home.jsx
@@ -185,7 +185,7 @@ export default function BasicCard() {
                 <TableHead >
                   <TableRow  >
                     <StyledTableCell>Notification No.</StyledTableCell>
-                    <StyledTableCell align="left">Data</StyledTableCell>
+                    <StyledTableCell align="left">Date/Time</StyledTableCell>
                     <StyledTableCell align="left">Well Location</StyledTableCell>
                     <StyledTableCell align="left">Well Installation</StyledTableCell>
                     <StyledTableCell align="left">Well number</StyledTableCell>
