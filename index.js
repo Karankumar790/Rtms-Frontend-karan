f1bc42a3b94ec8b6ace100e3d4993e017a15940e
Abhishek <adagoitsolutions@gmail.com>
2024-08-10 18:11:17 +0530

    Setup initial Code

---
+++ index.js
@@ -0,0 +1,20 @@
+const Express = require("express");
+const cors = require("cors");
+const path = require('path');
+const app = Express();
+const PORT = 80;
+
+app.use(cors());
+app.use(Express.static(path.join(__dirname, 'frontend')))
+
+app.get("/api", (req, res) => {
+    res.send({a: 2, b: 'Ramu', c: 6});
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log("Server Started On Post", PORT)
+})
