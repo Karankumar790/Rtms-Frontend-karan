9ff7970d89f5a6b5fad369b56331a81e814bfc6e
Abhishek <adagoitsolutions@gmail.com>
2024-08-11 13:07:08 +0530

    Update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(Express.static(path.join(__dirname, 'frontend')))
 
 app.get("/api", (req, res) => {
-    res.send({b: 59, c: 'mohan'});
+    res.send({b: 59, c: 'Mohan'});
 })
 
 app.get("*", (req, res) => {
