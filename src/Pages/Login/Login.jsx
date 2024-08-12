89b97059ae341c3be016521fac93185f1ea89559
keshav9761 <keshavtomar53@gmail.com>
2024-08-12 04:34:15 -0400

    remove ss

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -12,7 +12,7 @@ import CardContent from '@mui/joy/CardContent';
 function Login() {
     return (
         <PageContainer className='login-form-bg-image' showfooter showheader  >
-            <Grid container padding={4} display={'flex'} justifyContent={'start'} alignContent={'center'}>
+            <Grid container  padding={4}>
                     <Card >
                         <CardContent orientation="vertical">
                             <Grid item sx={{textAlign:"center"}} md={12} sm={12} xs={12}>
