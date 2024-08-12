3fc54a6c172ff922ba97c48fa594328f89cda930
keshav9761 <keshavtomar53@gmail.com>
2024-08-11 23:10:53 -0400

    CICD test

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -13,48 +13,48 @@ function Login() {
     return (
         <PageContainer className='login-form-bg-image' showfooter showheader  >
             <Grid container height={'100%'} display={'flex'} justifyContent={'start'} padding={5} alignContent={'center'}>
-                    <Card >
-                        <CardContent orientation="vertical">
-                            <Grid item sx={{textAlign:"center"}} md={12} sm={12} xs={12}>
-                                <Typography pt={5} fontSize="xxx-large">
-                                    Welcome
-                                </Typography>
-                                <Typography fontSize="large" color='#800000'>
-                                    Real Time Well Monitoring System
-                                </Typography>
+                <Card >
+                    <CardContent orientation="vertical">
+                        <Grid item sx={{ textAlign: "center" }} md={12} sm={12} xs={12}>
+                            <Typography pt={5} fontSize="xxx-large">
+                                Monu is a senior Developer
+                            </Typography>
+                            <Typography fontSize="large" color='#800000'>
+                                Real Time Well Monitoring System
+                            </Typography>
+                        </Grid>
+                        <form>
+                            <Grid container padding={1} spacing={1}>
+                                <Grid item sx={{ display: 'flex', alignItems: 'flex-end' }} md={12} sm={12} lg={12} xs={12}>
+                                    <AccountCircle sx={{ color: 'action.active', mr: 1 }} fontSize='large' />
+                                    <TextField className='custom-textfield' label="Username" variant="standard" fullWidth />
+                                </Grid>
+                                <Grid item sx={{ display: 'flex', alignItems: 'flex-end' }} md={12} sm={12} lg={12} xs={12}>
+                                    <HttpsIcon sx={{ color: 'action.active', mr: 1 }} fontSize='large' />
+                                    <TextField className='custom-textfield' label="Password" variant="standard" fullWidth />
+                                </Grid>
                             </Grid>
-                                <form>
-                                    <Grid container padding={1} spacing={1}>
-                                        <Grid item sx={{ display: 'flex', alignItems: 'flex-end' }} md={12} sm={12} lg={12} xs={12}>
-                                            <AccountCircle sx={{ color: 'action.active', mr: 1 }} fontSize='large' />
-                                            <TextField className='custom-textfield' label="Username" variant="standard" fullWidth />
-                                        </Grid>
-                                        <Grid item sx={{ display: 'flex', alignItems: 'flex-end' }} md={12} sm={12} lg={12} xs={12}>
-                                            <HttpsIcon sx={{ color: 'action.active', mr: 1 }} fontSize='large' />
-                                            <TextField className='custom-textfield' label="Password" variant="standard" fullWidth />
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container padding={1} spacing={1} direction='column' py={1}>
-                                        <Grid item textAlign={'end'} >
-                                            <Link to="/forgot" style={{ textDecoration: "none" }}>
-                                                Forgot Password
-                                            </Link>
-                                        </Grid>
-                                        <Grid item>
-                                            <Link to="/dashboard">
-                                                <Button variant='contained' className='btn-primary' fullWidth type='submit'>Login</Button>
-                                            </Link>
-                                        </Grid>
-                                        <Grid item textAlign="center">
-                                            <Typography fontSize="small">Don't have an account?
-                                                <Link to='/signup' style={{ textDecoration: "none"}}> Sign Up</Link>
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-                                </form>
-                        </CardContent>
-                    </Card>
-                </Grid>
+                            <Grid container padding={1} spacing={1} direction='column' py={1}>
+                                <Grid item textAlign={'end'} >
+                                    <Link to="/forgot" style={{ textDecoration: "none" }}>
+                                        Forgot Password
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link to="/dashboard">
+                                        <Button variant='contained' className='btn-primary' fullWidth type='submit'>Login</Button>
+                                    </Link>
+                                </Grid>
+                                <Grid item textAlign="center">
+                                    <Typography fontSize="small">Don't have an account?
+                                        <Link to='/signup' style={{ textDecoration: "none" }}> Sign Up</Link>
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
         </PageContainer>
     )
 }
