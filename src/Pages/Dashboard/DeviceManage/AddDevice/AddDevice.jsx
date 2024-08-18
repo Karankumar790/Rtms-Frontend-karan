2f5102f8ee12d00a8bdd030c83295fd568c5d319
keshav9761 <keshavtomar53@gmail.com>
2024-08-17 20:49:40 -0400

    master

--- src/Pages/Dashboard/DeviceManage/AddDevice/AddDevice.jsx
+++ src/Pages/Dashboard/DeviceManage/AddDevice/AddDevice.jsx
@@ -1,12 +1,11 @@
-import {
-  Button,
+import React from "react";
+import { Button,
   FormControlLabel,
   Grid,
   TextField,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
 import FormGroup from "@mui/material/FormGroup";
 // import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from "@mui/material/Checkbox";
