77babf12d28cf3cc60be30b4747830df3b1f0581
Karan Verma <foxboroinstrument@gmail.com>
2024-10-01 11:52:09 +0530

    Update Forgot.jsx

--- src/Pages/Forgot/Forgot.jsx
+++ src/Pages/Forgot/Forgot.jsx
@@ -8,52 +8,24 @@ import { setForgotDetails } from "../../apis/authSlice";
 import { toast } from "react-toastify";
 
 export default function Forgot() {
-    return (
-        <PageContainer showheader showfooter display="flex"  alignItems='center' className='bgImg'  justifyContent='center'>
-            {/* <Grid container > */}
-                <Paper sx={{ borderRadius: '20px', mx:'5%', width: '45rem' }}>
-                    <Grid item p={3} >
-                        <form>
-                            <Grid item mt={2}>
-
-                                <Typography variant='h5' fontSize='x-large' textAlign="center">Forgot Your Password?</Typography>
-                            </Grid>
-                            <Grid item mt={2}>
-                                
-                                <Typography fontSize='large' textAlign="center">Enter Your Registered Email. </Typography>
-                            </Grid>
-                            <Grid item mt={3}>
-                                <TextField justifyContent="center" label="Enter Email Address"  variant="outlined" size='small' fullWidth />
-                            </Grid>
-                            <Grid item mt={3} >
-                                <Link to="/Reset"><Button variant='contained' color='primary' fullWidth type='submit'>Send Verification Code</Button></Link>
-                            </Grid>
-                            <Grid item mt={2} textAlign='center'>
-                                <Link to="/" style={{ textDecoration: "none" }}>Back to Login</Link>
-                            </Grid>
-                        </form>
-                    </Grid>
-                </Paper>
-            {/* </Grid> */}
-        </PageContainer>
-    )
-}
+  // State for form inputs
   const [formValues, setFormValues] = useState({ email: "" });
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // Handle input change
   const handleInputs = (e) => {
-    setFormValues((pre) => ({ ...pre, [e.target?.name]: e.target?.value }));
+    setFormValues((prev) => ({ ...prev, [e.target?.name]: e.target?.value }));
   };
 
+  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    //integration
     try {
       const response = await forgotPassword(formValues);
       if (response.success) {
-        //store data in redux store
+        // Store data in redux
         dispatch(
           setForgotDetails({
             email: formValues.email,
@@ -123,4 +95,4 @@ export default function Forgot() {
       </Paper>
     </PageContainer>
   );
-
+}
