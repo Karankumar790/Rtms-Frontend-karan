b6e68220874ea7e3f20309fdbe953c609e6853b1
karankumar790 <karan20-cs@sanskar.org>
2024-11-07 17:54:55 +0530

    monitor view page done

--- src/Pages/Dashboard/ManageAsset.jsx/OrgMessageForward.jsx
+++ src/Pages/Dashboard/ManageAsset.jsx/OrgMessageForward.jsx
@@ -30,38 +30,6 @@ import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import EmailIcon from '@mui/icons-material/Email';
 
-import {
-  FormControl,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Paper,
-  Select,
-  TextField,
-  Typography,
-} from "@mui/material";
-import React, { useState } from "react";
-import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateTimePicker, TimeField, TimePicker } from "@mui/x-date-pickers";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import dayjs from "dayjs";
-import { Box } from "@mui/system";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import RestoreIcon from "@mui/icons-material/Restore";
-import ThreePIcon from "@mui/icons-material/ThreeP";
-import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
-import EmailIcon from '@mui/icons-material/Email';
-
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
