f2441c775ada47e8c312b16916058144abec75f3
karankumar790 <karan20-cs@sanskar.org>
2024-12-31 17:23:13 +0530

    home

--- src/Pages/Dashboard/Home/Home.jsx
+++ src/Pages/Dashboard/Home/Home.jsx
@@ -44,7 +44,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityIcon from "@mui/icons-material/Visibility";  
 import Modal from "@mui/material/Modal";
 import { borderRadius } from "@mui/system";
 import { TextareaAutosize as BaseTextareaAutosize } from "@mui/base/TextareaAutosize";
